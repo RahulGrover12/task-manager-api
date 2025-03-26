f92cce175202bcad42f744396c14bc877c0f6f6d
Rahul Grover <groverrahul47@gmail.com>
2025-03-26 15:05:50 +0530

    google style guide added

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+export default {
+  root: true,
+  extends: ["google"],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
+  rules: {
+    "require-jsdoc": "off",
+    quotes: ["error", "single"],
+    indent: ["error", 2],
+  },
+};
